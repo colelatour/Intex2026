2cb62a0286eab1495bce3e0b46db28a5aa1d1bdd
colelatour <clatour0@byu.edu>
2026-04-07 11:10:05 -0600

    Merge fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
 // src/App.tsx
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar  from './components/layout/Navbar';
-import Home    from './pages/Home';
-import Donate  from './pages/Donate';
-import Admin   from './pages/Admin';
-import Login   from './pages/Login';
 import Navbar          from './components/layout/Navbar';
 import Home            from './pages/Home';
 import Donate          from './pages/Donate';
 import Admin           from './pages/Admin';
+import Login           from './pages/Login';
 import ImpactDashboard from './pages/ImpactDashboard';
 
 export default function App() {
@@ -17,13 +13,10 @@ export default function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/"       element={<Home />}   />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="/admin"  element={<Admin />}  />
-        <Route path="/login" element={<Login />}  />
         <Route path="/"       element={<Home />}            />
         <Route path="/donate" element={<Donate />}          />
         <Route path="/admin"  element={<Admin />}           />
+        <Route path="/login"  element={<Login />}           />
         <Route path="/impact" element={<ImpactDashboard />} />
       </Routes>
     </>
