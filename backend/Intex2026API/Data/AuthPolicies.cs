03dc5327ad5d8fe46644be0fa7c7c6ecbc8da293
colelatour <clatour0@byu.edu>
2026-04-09 13:15:51 -0600

    security fixes

---
+++ backend/Intex2026API/Data/AuthPolicies.cs
@@ -0,0 +1,8 @@
+namespace Intex2026API.Data;
+
+public static class AuthPolicies
+{
+    public const string AdminOnly = "AdminOnly";
+    public const string AdminWorker = "AdminWorker";
+    public const string DonorAccess = "DonorAccess";
+}
---
+++ backend/Intex2026API/Data/AuthRoles.cs
@@ -0,0 +1,8 @@
+namespace Intex2026API.Data;
+
+public static class AuthRoles
+{
+    public const string Admin = "Admin";
+    public const string Worker = "Worker";
+    public const string Donor = "Donor";
+}
